2be6bddd0f89de1a4c4977671388e7f63bae39b0
rubenpieters <rubenpietersgithub@gmail.com>
2019-11-02 11:28:05 +0100

    add basic nodes

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,14 @@
 import * as PIXI from "pixi.js";
+import { Layout, GenerateNode, GameNode, GameState, advanceState } from "../shared/game";
 
 let currentTime = new Date().getTime();
 let prevTime = currentTime;
 let deltaTime = 0;
 
-let timer = 0;
-
 window.addEventListener("load", main);
 window.requestAnimationFrame(update);
 
-let text = new PIXI.Text(`Timer: ${timer}`, {
+const text = new PIXI.Text(`Index: 0 Time: 0`, {
     fontFamily : 'Arial',
     fontSize: 24,
     fill: 0xff1010,
@@ -17,7 +16,34 @@ let text = new PIXI.Text(`Timer: ${timer}`, {
 });
 text.x = 200;
 text.y = 200;
-  
+
+const barTexture = PIXI.Texture.from("assets/sprites/bar.png");
+const bar = new PIXI.Sprite(barTexture);
+
+const layout: Layout = [
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+    new GenerateNode("x"),
+  ];
+
+const state: GameState = {
+  nodeIndex: 0,
+  timeInNode: 0,
+  layout,
+  runes: {},
+};
+
 function main(): void {
     const app = new PIXI.Application({width: 540, height: 960});
     
@@ -26,23 +52,30 @@ function main(): void {
     const container = new PIXI.Container();
     app.stage.addChild(container);
 
-    const texture = PIXI.Texture.from("assets/sprites/box.png");
-    for (let i = 0; i < 14; i++) {
-        const box = new PIXI.Sprite(texture);
+    const boxTexture = PIXI.Texture.from("assets/sprites/box.png");
+    let i = 0;
+    layout.forEach((node: GameNode) => {const box = new PIXI.Sprite(boxTexture);
         box.x = (i % 7) * 55 + 50;
         box.y = Math.floor(i / 7) * 55 + 50;
         container.addChild(box);
-    }
+        i += node.size;
+    });
 
     container.addChild(text);
 
+    bar.x = 50;
+    bar.y = 45;
+    container.addChild(bar);
 }
 
 function update(): void {
+    prevTime = currentTime;
     currentTime = new Date().getTime();
-    deltaTime = (currentTime - prevTime) / 1000;
-    timer += deltaTime;
-    if (timer >= 1000) timer = timer - 1000 * (Math.floor(timer / 1000));
-    text.text = `Timer: ${Math.floor(timer)}`;
+    deltaTime = (currentTime - prevTime) / 5;
+    advanceState(state, deltaTime);
+    text.text = `Index: ${state.nodeIndex} Time: ${Math.floor(state.timeInNode)} X: ${state.runes.x}`;
+    const timerValue = state.nodeIndex + state.timeInNode / 100;
+    bar.x = (timerValue % 7 / 7) * (50 * 7 + 5 * 6) + 50;
+    bar.y = Math.floor(timerValue / 7) * 55 + 45;
     requestAnimationFrame(update);
 }
---
+++ src/shared/game.ts
@@ -0,0 +1,68 @@
+export class GenerateNode {
+  public readonly tag: "GenerateNode" = "GenerateNode";
+  public readonly size = 1;
+
+  constructor(
+    public readonly rune: string,
+  ) {}
+}
+
+export class Empty {
+  public readonly tag: "Empty" = "Empty";
+  public readonly size = 1;
+
+  constructor(
+  ) {}
+}
+
+export type GameNode
+  = GenerateNode
+  | Empty
+  ;
+
+export type Layout = GameNode[];
+
+export type GameState = {
+  nodeIndex: number,
+  timeInNode: number,
+  layout: Layout,
+  runes: any,
+};
+
+export function advanceState(
+  state: GameState,
+  delta: number,
+) {
+  let newTimeInNode = state.timeInNode + delta;
+  let activationThreshold = state.layout[state.nodeIndex].size * 100;
+  while (newTimeInNode > activationThreshold) {
+    // activate node
+    activateNode(state.layout[state.nodeIndex], state);
+    // update node index
+    state.nodeIndex = state.nodeIndex + state.layout[state.nodeIndex].size;
+    if (state.nodeIndex >= 14) state.nodeIndex -= 14 * (Math.floor(state.nodeIndex / 14));
+    // calculate new threshold and update new time in node
+    activationThreshold = state.layout[state.nodeIndex].size * 100;
+    newTimeInNode = newTimeInNode - activationThreshold;
+  }
+  state.timeInNode = newTimeInNode;
+}
+
+export function activateNode(
+  node: GameNode,
+  state: GameState,
+): void {
+  switch (node.tag) {
+    case "GenerateNode": {
+      if (typeof state.runes[node.rune] === "undefined") {
+        state.runes[node.rune] = 1;
+      } else {
+        state.runes[node.rune] += 1;
+      }
+      break;
+    }
+    case "Empty": {
+      break;
+    }
+  }
+}
